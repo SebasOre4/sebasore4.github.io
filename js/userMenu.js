1a168bfe2ae754b8c0df53c343dd62fdbf057718
SebasOre4 <asorellana4@gmail.com>
2022-02-07 20:19:22 -0500

    Start of PersonalPageTemplate

--- js/userMenu.js
+++
@@ -1,9 +0,0 @@
-var userMenu = document.getElementById('user-menu').classList;
-
-function controlUserMenu(){
-    if (userMenu.contains('user-menu--on')) {
-        userMenu.remove('user-menu--on');
-    }else{
-        userMenu.add('user-menu--on');
-    }
-}
